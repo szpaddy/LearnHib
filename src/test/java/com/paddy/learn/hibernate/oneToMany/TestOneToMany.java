76539126fceddd7215e2ff11e08b2d54406d238e
paddy <paddy@home>
2015-08-01 11:27:18 +0800

    sync data

--- src/test/java/com/paddy/learn/hibernate/oneToMany/TestOneToMany.java
+++ src/test/java/com/paddy/learn/hibernate/oneToMany/TestOneToMany.java
@@ -1,6 +1,8 @@
 package com.paddy.learn.hibernate.oneToMany;
 
 import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
@@ -11,8 +13,8 @@ import org.junit.Test;
 
 public class TestOneToMany {
 
-	@Test
-	public void testOneToMany() throws SQLException {
+	//@Test
+	public void testOneToManyOK() throws SQLException {
 		try {
 			SessionFactory sf = new AnnotationConfiguration().configure()
 					.buildSessionFactory();
@@ -36,36 +38,42 @@ public class TestOneToMany {
 			st2.setClasses(classes);
 			session.save(st2);
 			tx.commit();
-			/*
-			 * 输出如下: Hibernate: insert into classes (name) values (?) Hibernate:
-			 * insert into student (classid, sname) values (?, ?) Hibernate:
-			 * insert into student (classid, sname) values (?, ?)
-			 */
+
+		} catch (HibernateException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testOneToManyError() throws SQLException {
+		try {
+			SessionFactory sf = new AnnotationConfiguration().configure()
+					.buildSessionFactory();
+			Session session = sf.openSession();
+			Transaction tx = session.beginTransaction();
+
 			/*
 			 * 因为一端维护关系另一端不维护关系的原因，我们必须注意避免在应用中用不维护关系的类(class)建立关系，
 			 * 因为这样建立的关系是不会在数据库中存储的。 如上的代码倒过来,则插入时,student的外键值为空.如下:
 			 */
-			// Student st1=new Student();
-			// st1.setSname("jason");
-			// session.save(st1);
-			//
-			// Student st2=new Student();
-			// st2.setSname("hwj");
-			// session.save(st2);
-			//
-			// Set<Student> students=new HashSet<Student>();
-			// students.add(st1);
-			// students.add(st2);
-			//
-			// Classes classes=new Classes();
-			// classes.setName("access");
-			// classes.setStudents(students);
-			// session.save(classes);
-			/*
-			 * 输出如下: Hibernate: insert into student (classid, sname) values (?,
-			 * ?) Hibernate: insert into student (classid, sname) values (?, ?)
-			 * Hibernate: insert into classes (name) values (?)
-			 */
+			Student st1 = new Student();
+			st1.setSname("jason");
+			session.save(st1);
+
+			Student st2 = new Student();
+			st2.setSname("hwj");
+			session.save(st2);
+
+			Set<Student> students = new HashSet<Student>();
+			students.add(st1);
+			students.add(st2);
+
+			Classes classes = new Classes();
+			classes.setName("access");
+			classes.setStudents(students);
+			session.save(classes);
+
+			tx.commit();
 		} catch (HibernateException e) {
 			e.printStackTrace();
 		}
