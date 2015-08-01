c5aba5fb134e46fee3ad4bb1b836adabb2f88dcf
szpaddy <251287093@qq.com>
2015-08-01 17:31:55 +0800

    sync data

--- src/test/java/com/paddy/learn/hibernate/oneToMany/TestOneToMany.java
+++ src/test/java/com/paddy/learn/hibernate/oneToMany/TestOneToMany.java
@@ -9,22 +9,20 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.AnnotationConfiguration;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class TestOneToMany {
 
-	//@Test
+	@Test
 	public void testOneToManyOK() throws SQLException {
+		Session session = null;
 		try {
 			SessionFactory sf = new AnnotationConfiguration().configure()
 					.buildSessionFactory();
-			Session session = sf.openSession();
+			session = sf.openSession();
 			Transaction tx = session.beginTransaction();
-			/*
-			 * 因为mappedBy是定义在classes中,即classes类不负责维护级联关系.即维护者是student.所以,
-			 * 1.要将clsses的数据,赋给student,即用student的setClasses()方法去捆定class数据;
-			 * 2.在进行数据插入/更新session.save()/session.update()时,最后操作的是student.
-			 */
+
 			Classes classes = new Classes();
 			classes.setName("access");
 
@@ -41,41 +39,10 @@ public class TestOneToMany {
 
 		} catch (HibernateException e) {
 			e.printStackTrace();
-		}
-	}
-
-	@Test
-	public void testOneToManyError() throws SQLException {
-		try {
-			SessionFactory sf = new AnnotationConfiguration().configure()
-					.buildSessionFactory();
-			Session session = sf.openSession();
-			Transaction tx = session.beginTransaction();
-
-			/*
-			 * 因为一端维护关系另一端不维护关系的原因，我们必须注意避免在应用中用不维护关系的类(class)建立关系，
-			 * 因为这样建立的关系是不会在数据库中存储的。 如上的代码倒过来,则插入时,student的外键值为空.如下:
-			 */
-			Student st1 = new Student();
-			st1.setSname("jason");
-			session.save(st1);
-
-			Student st2 = new Student();
-			st2.setSname("hwj");
-			session.save(st2);
-
-			Set<Student> students = new HashSet<Student>();
-			students.add(st1);
-			students.add(st2);
-
-			Classes classes = new Classes();
-			classes.setName("access");
-			classes.setStudents(students);
-			session.save(classes);
-
-			tx.commit();
-		} catch (HibernateException e) {
-			e.printStackTrace();
+		} finally {
+			if (session != null) {
+				session.close();
+			}
 		}
 	}
 }
